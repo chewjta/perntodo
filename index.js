45d8ceb11046916aa6ea18727787ff3a1deeebf1
chewjta <chewjta@gmail.com>
2020-09-15 03:19:40 +0800

    added heroku

--- index.js
+++ index.js
@@ -80,7 +80,9 @@ app.delete('/todos/:id', async (req,res) => {
     }
 })
 
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
 
 app.listen(PORT, ()=>{
     console.log(`server has started on port ${PORT}`)
